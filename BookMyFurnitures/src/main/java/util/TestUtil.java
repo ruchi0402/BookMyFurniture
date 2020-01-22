acebc39cf3aff72d0656d20245f28d9ef6638e3a
x216706 <x216706@SY4-F2ZDLR2.LUV.AD.SWACORP.com>
2020-01-21 18:41:04 -0600

    Created Common Constant Class

--- BookMyFurnitures/src/main/java/util/TestUtil.java
+++ BookMyFurnitures/src/main/java/util/TestUtil.java
@@ -4,20 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import base.CommonConstant;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
 import base.BasePage;
-
-public class TestUtil extends BasePage {
+import base.CommonConstant;
+public class TestUtil extends BasePage  {
 
 	public static String TESTDATA_SHEET_PATH = "C:\\Users\\Admin\\Desktop\\SDET\\BookMyFurnitures\\src\\main\\java\\testdata\\Datasheet.xls";
 
@@ -27,7 +21,6 @@ public class TestUtil extends BasePage {
 		FileUtils.copyFile(scrFile,
 				new File(currentDir + "/test-output/screenshots/" + System.currentTimeMillis() + ".png"));
 	}
-
 	public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
 		FileInputStream file = null;
 		try {
@@ -36,19 +29,16 @@ public class TestUtil extends BasePage {
 			e.printStackTrace();
 		}
 		try {
-			book = new HSSFWorkbook(file);
+			CommonConstant.book = new HSSFWorkbook(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		sheet = book.getSheet(sheetName);
-		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
-		// System.out.println(sheet.getLastRowNum() + "--------" +
-		// sheet.getRow(0).getLastCellNum());
-		for (int i = 0; i < sheet.getLastRowNum(); i++) {
-			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
-				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
-				System.out.println(data[i][k]);
-			}
+		CommonConstant.sheet = CommonConstant.book.getSheet(sheetName);
+		Object[][] data = new Object[CommonConstant.sheet.getLastRowNum()][CommonConstant.sheet.getRow(0).getLastCellNum()];
+		for (int i = 0; i < CommonConstant.sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < CommonConstant.sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = CommonConstant.sheet.getRow(i + 1).getCell(k).toString();
+				}
 		}
 		return data;
 	}
