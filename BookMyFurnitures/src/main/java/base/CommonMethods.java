216cde76654366e5a2ec22b8c33cade17b72de1a
Admin <Admin@E4ML14565>
2020-02-15 22:09:22 -0600

    # WARNING: head commit changed in the meantime
    test

--- BookMyFurnitures/src/main/java/base/CommonMethods.java
+++ BookMyFurnitures/src/main/java/base/CommonMethods.java
@@ -3,6 +3,8 @@ package base;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonMethods extends BaseClass {
 
@@ -15,13 +17,13 @@ public class CommonMethods extends BaseClass {
 			driver.findElement(locator).click();
 		} catch (Exception e) {
 			log.error(e.getMessage());
-			throw (e);
+			assert false;
 		}
 	}
 
 	public void click(WebElement webElement) {
 		try {
-			webElement.click();
+			webDriverWait(webElement).click();
 		} catch (Exception e) {
 			log.error(e.getMessage());
 			assert false;
@@ -31,7 +33,7 @@ public class CommonMethods extends BaseClass {
 //sendKeys method checks the presence the webelement using try/catch block and performs operation if available.
 	public void sendKeys(WebElement webElement, String name) {
 		try {
-			webElement.sendKeys(name);
+		webDriverWait(webElement).sendKeys(name);
 		} catch (Exception e) {
 			log.error(e.getMessage());
 			assert false;
@@ -41,11 +43,24 @@ public class CommonMethods extends BaseClass {
 
 	public void isDisplayed(WebElement webElement, String message) {
 		try {
-			webElement.isDisplayed();
+			webDriverWait(webElement).isDisplayed();
 			log.info(message);
 		} catch (Exception e) {
 			log.error(e.getMessage());
 			assert false;
 		}
 	}
+	
+	public WebElement webDriverWait(WebElement webElement) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver,10);
+			wait.until(ExpectedConditions.visibilityOf(webElement));
+			} catch (Exception e) {
+			log.error(e.getMessage());
+			assert false;
+		}
+		return webElement;
+		
+	}
+	
 }
