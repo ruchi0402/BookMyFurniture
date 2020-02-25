d4d0031cd1961efbdb1fdac9f8c3d8b316ca9fd0
Admin <Admin@E4ML14565>
2020-02-24 23:28:08 -0600

    commit

--- BookMyFurnitures/src/main/java/reports/TestNgListenersApi.java
+++ BookMyFurnitures/src/main/java/reports/TestNgListenersApi.java
@@ -17,7 +17,7 @@ public class TestNgListenersApi extends TestListenerAdapter {
 
 	public void onStart(ITestContext testContext) {
 		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")
-				+ "\\src\\test\\java\\reports\\ApiAutomationReport" + System.currentTimeMillis() + ".html");
+				+ "/test-output/RestApiAutomation/RestApiAutomationReport" + System.currentTimeMillis() + ".html");
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
 		extent.setSystemInfo("Environment", "QA");
