516c1a5e91899148bc168c905307b69d5c844bc2
ruchi0402 <59716665+ruchi0402@users.noreply.github.com>
2020-02-29 23:42:50 -0600

    Update CommonConstant.java

--- BookMyFurnitures/src/main/java/base/CommonConstant.java
+++ BookMyFurnitures/src/main/java/base/CommonConstant.java
@@ -11,4 +11,7 @@ public class CommonConstant {
 	public static final long IMPLICIT_WAIT = 20;
 	public static final String getURL = "http://okmry52647dns.eastus.cloudapp.azure.com:8089/rest/api/product/all";
 	public static final String postURL = "http://okmry52647dns.eastus.cloudapp.azure.com:8089/rest/api/product/";
+	public static final String DB_URL = "jdbc:mysql://okmry52647dns.eastus.cloudapp.azure.com:3306/bmf-abhishek-jun19-dev";
+	public static final String DB_USER = "sdetuser1";
+	public static final String DB_PASSWORD = "Welcome123$";
 }
